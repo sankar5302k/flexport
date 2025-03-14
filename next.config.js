e13e6816aef7fc718dd7d1195b53adeef9cb0667
sankaran 2006 <viji.senthil2013@gmail.com>
2025-03-14 19:52:40 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ module.exports = {
       ,'www.nojitter.com','f5b623aa.rocketcdn.me','www.adhamdannaway.com','media.licdn.com','api.microlink.io'
     ],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
